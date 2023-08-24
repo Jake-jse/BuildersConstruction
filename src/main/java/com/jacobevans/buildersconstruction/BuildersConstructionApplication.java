0bbfa6a413735d35e5b725b24f6c1d6bd4d3d329
jake e <jse288907@gmail.com>
2023-08-23 20:06:48 -0400

    init commit

---
+++ src/main/java/com/jacobevans/buildersconstruction/BuildersConstructionApplication.java
@@ -0,0 +1,13 @@
+package com.jacobevans.buildersconstruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildersConstructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildersConstructionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacobevans/buildersconstruction/BuildersConstructionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobevans.buildersconstruction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildersConstructionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
